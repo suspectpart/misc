19de24b9d0486d49d7675109d2d84a349dced733
Horst Schneider <horst.schneider@posteo.de>
2014-04-29 15:47:34 +0200

    added elasticsearch installation as a service

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # install java (prerequisite)
 apt-get install openjdk-7-jdk -y
+apt-get install chkconfig
 
 # install logstash and elasticsearch
 cd /opt
@@ -11,6 +12,16 @@ tar xvzf elasticsearch-1.0.1.tar.gz
 rm logstash-1.4.0.tar.gz
 rm elasticsearch-1.0.1.tar.gz
 
+# install and start elasticsearch as a service
+cd elasticsearch-1.0.1/
+wget --no-check-certificate 'https://github.com/elasticsearch/elasticsearch-servicewrapper/archive/master.zip' -0 elasticsearch-servicewrapper.zip
+unzip elasticsearch-servicewrapper.zip
+mv elasticsearch-servicewrapper-master/service bin/
+rm elasticsearch-servicewrapper.zip
+bin/service/elasticsearch install
+chkconfig --add elasticsearch
+service elasticsearch start
+
 #start logstash
-./logstash-1.4.0/bin/logstash -f logstash.conf
-./elasticsearch-1.0.1/bin/elasticsearch
+#./logstash-1.4.0/bin/logstash -f logstash.conf
+#./elasticsearch-1.0.1/bin/elasticsearch
