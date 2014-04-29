07d11bf6cb88bd63b30d1e0871f41ca2d05503f5
Horst Schneider <horst.schneider@posteo.de>
2014-04-29 16:14:57 +0200

    changed install script

--- install.sh
+++ install.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 # install java (prerequisite)
-apt-get install openjdk-7-jdk chkconfig unzip -y
+apt-get install openjdk-7-jdk -y
 
-# install logstash and elasticsearch
-cd /opt
-#wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz
-#wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz
-#tar xvzf logstash-1.4.0.tar.gz
-#tar xvzf elasticsearch-1.0.1.tar.gz
-#rm logstash-1.4.0.tar.gz
-#rm elasticsearch-1.0.1.tar.gz
-
-# install and start elasticsearch as a service
-#cd elasticsearch-1.0.1/
-#wget --no-check-certificate 'https://github.com/elasticsearch/elasticsearch-servicewrapper/archive/master.zip' -0 elasticsearch-servicewrapper.zip
-#unzip elasticsearch-servicewrapper.zip
-#mv elasticsearch-servicewrapper-master/service bin/
-#rm elasticsearch-servicewrapper.zip
-#bin/service/elasticsearch install
-#chkconfig --add elasticsearch
-#service elasticsearch start
+# install elasticsearch and logstash
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.deb
-dpkg -i elasticsearch-1.1.0.deb
 wget https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.0-1-c82dc09_all.deb
 dpkg -i logstash_1.4.0-1-c82dc09_all.deb
+dpkg -i elasticsearch-1.1.0.deb
+rm logstash_1.4.0-1-c82dc09_all.deb
+rm elasticsearch-1.1.0.deb
+
+# copy logstash.conf to /etc/logstash/conf.d and adjust rights
+cp logstash.conf /etc/logstash/conf.d/
+chmod root:root logstash.conf
+chown 744 logstash.conf
+
+#start services
+service logstash restart
+service logstash force-reload 
 service elasticsearch restart
-#start logstash
-#./logstash-1.4.0/bin/logstash -f logstash.conf
-#./elasticsearch-1.0.1/bin/elasticsearch
