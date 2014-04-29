1414ee9516be35741f30c8c4f54dad713203c384
Horst Schneider <horst.schneider@posteo.de>
2014-04-29 15:31:19 +0200

    added stuff to logstash install and a logstash config file

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# install java (prerequisite)
 apt-get install openjdk-7-jdk -y
+
+# install logstash and elasticsearch
+cd /opt
 wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz
 tar xvzf logstash-1.4.0.tar.gz
-mv logstash-1.4.0/ /opt/logstash-1.4.0
 rm logstash-1.4.0.tar.gz
-/opt/logstash-1.4.0/bin/logstash -e 'input { stdin { } } output { stdout { } }'
+curl -0 https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz
+tar xvzf elasticsearch-1.0.1.tar.gz
+rm elasticsearch-1.0.1.tar.gz
+
+#start logstash
+./logstash-1.4.0/bin/logstash -f logstash.conf
+./elasticsearch-1.0.1/bin/elasticsearch
