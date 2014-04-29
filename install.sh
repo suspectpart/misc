20f5e14de773003d5f3cba7eba116d085e18eadf
Horst Schneider <horst.schneider@posteo.de>
2014-04-29 15:23:07 +0200

    changed curl to wget

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ apt-get install openjdk-7-jdk -y
 # install logstash and elasticsearch
 cd /opt
 wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz
 tar xvzf logstash-1.4.0.tar.gz
-rm logstash-1.4.0.tar.gz
-curl -0 https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz
 tar xvzf elasticsearch-1.0.1.tar.gz
+rm logstash-1.4.0.tar.gz
 rm elasticsearch-1.0.1.tar.gz
 
 #start logstash
