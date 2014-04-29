4197fb1a394a060e90da01724b8421c39042cb59
Horst Schneider <horst.schneider@posteo.de>
2014-04-29 17:37:54 +0200

    also added logstash as deb package

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ apt-get install openjdk-7-jdk chkconfig unzip -y
 
 # install logstash and elasticsearch
 cd /opt
-wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz
+#wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz
 #wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz
-tar xvzf logstash-1.4.0.tar.gz
+#tar xvzf logstash-1.4.0.tar.gz
 #tar xvzf elasticsearch-1.0.1.tar.gz
-rm logstash-1.4.0.tar.gz
+#rm logstash-1.4.0.tar.gz
 #rm elasticsearch-1.0.1.tar.gz
 
 # install and start elasticsearch as a service
@@ -22,7 +22,8 @@ rm logstash-1.4.0.tar.gz
 #service elasticsearch start
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.deb
 dpkg -i elasticsearch-1.1.0.deb
-
+wget https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.0-1-c82dc09_all.deb
+dpkg -i logstash_1.4.0-1-c82dc09_all.deb
 service elasticsearch restart
 #start logstash
 #./logstash-1.4.0/bin/logstash -f logstash.conf
